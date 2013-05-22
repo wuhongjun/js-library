6d0ecf2cd027ee3ac1efc299f1f16bcca5c44244
zenxds <zenxds@gmail.com>
2013-05-22 22:04:28 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+  // Initializes the Grunt tasks with the following settings
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // A list of files, which will be syntax-checked by JSHint
+    jshint: {
+      all: ['src/*.js']
+    },
+
+    less: {
+      compile: {
+        files: {
+          'assets/css/main.css': 'src/less/main.less'
+        }
+      }
+    },
+
+    // Files to be concatenated … (source and destination files)
+    concat: {
+      options: {
+        separator: ';',
+        banner: '/*! <%= pkg.name %> -- <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      }, 
+      dist: {
+        src: ['src/src1.js', 'src/src2.js', 'src/src3.js'],
+        dest: 'dist/built.js'
+      },
+      build: {
+        src: ['src/src1.js', 'src/src2.js'],
+        dest: 'build/built.js'
+      }
+    },
+
+    // … and minified (source and destination files)
+    uglify: {
+      build: {
+        files: {
+          'dist/built.min.js': ['dist/built.js']
+        }
+      }
+    },
+
+    // Tasks being executed with 'grunt watch'
+    watch: {
+      js: {
+        files: 'src/*.js',
+        tasks: ['jshint', 'concat', 'uglify']
+      },
+      less: {
+        files: 'src/less/*.less',
+        tasks: ['less']
+      }
+    }
+  });
+
+  // Load the plugins that provide the tasks we specified in package.json.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // This is the default task being executed if Grunt
+  // is called without any further parameter.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'less', 'watch']);
+
+};
--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 (function(seajs) { 
 	var alias = {
-		jquery: 'sea-modules/gallery/jquery/1.8.3/jquery.js',
-		underscore: 'sea-modules/gallery/underscore/1.4.3/underscore.js',
-		marked: 'sea-modules/gallery/marked/0.2.4/marked.js',
-		mocha: 'sea-modules/gallery/mocha/1.7.3/mocha.js',
-		less: 'sea-modules/gallery/less/1.3.1/less.js',
+		jquery: 'gallery/jquery/1.8.3/jquery.js',
 
 		satyr: 'seed.js',
 		lang: 'src/lang.js',
@@ -21,29 +17,21 @@
 
 	seajs.config({
 		debug: true,
+
+		charset: 'utf-8',
+
 		alias: alias,
-		preload: ['jquery']
+		
+		// 预加载项
+		preload: [
+			Function.prototype.bind ? '' : 'es5-safe',
+			this.JSON ? '' : 'json'
+		],
+
+		paths: {
+			'gallery': 'sea-modules/gallery'
+		},
+
+		plugins: ['text', 'shim']
 	});
-
-	// seajs.use('less', function(less) {
-	// 	console.log(less);
-	// });
-	// seajs.use(['satyr', 'jquery','valueChange'], function(S, $, valueChange) {
-	// 	$.valueChange('#textarea', function(p, n) {
-	// 		console.log(p);
-	// 		console.log(n);
-	// 	});
-	// });
-	// seajs.use(['satyr', 'underscore', 'marked', 'lang', 'cookie', 'encode', 'time', 'support', 'random', 'log', 'hashlib', 'sort'], function(S, _, marked) {
-	// 	console.log(S);
-	// 	S.assert('abc', 'fail in assert');
-	// 	S.assertEqual('a', 0, 'fail in assertEqual');
-	// 	S.log(S.hex_md5('abc'));
-	// 	S.log(marked('# h1'));
-	// 	_.each([1, 2, 3], function(value, index, arr) {
-	// 		S.log(value);
-	// 	});
-
-	// 	S.log(S.insertSort([2, 5, 1, 6, 4, 7, 9]));
-	// });
 })(seajs);
